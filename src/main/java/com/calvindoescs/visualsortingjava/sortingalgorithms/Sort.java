849dd15b87512ff940cf2cc3a035885d3d50c25a
CalvinDoesCS <calvinntran.cs@gmail.com>
2024-01-22 17:52:47 -0500

    Refactor

--- src/main/java/com/calvindoescs/visualsortingjava/sortingalgorithms/Sort.java
+++ src/main/java/com/calvindoescs/visualsortingjava/sortingalgorithms/Sort.java
@@ -4,44 +4,49 @@ import com.calvindoescs.visualsortingjava.RectNode;
 import com.calvindoescs.visualsortingjava.VisualSortController;
 import javafx.animation.FillTransition;
 import javafx.animation.ParallelTransition;
+import javafx.animation.SequentialTransition;
 import javafx.animation.Transition;
 import javafx.scene.paint.Color;
 import javafx.util.Duration;
 
 import java.util.ArrayList;
 import java.util.List;
+
 public abstract class Sort {
     final Color START_COLOR = Color.LIGHTBLUE;
     final Color SELECT_COLOR = Color.CYAN;
     final Color SORTED_COLOR = Color.ROYALBLUE;
-    ParallelTransition colorRectNode(RectNode[] arr, Color color, int...a) {
+
+    ParallelTransition colorRectNode(RectNode[] arr, Color color, int... a) {
         ParallelTransition pt = new ParallelTransition();
 
         for (int i = 0; i < a.length; i++) {
-            FillTransition fillTransition= new FillTransition();
+            FillTransition fillTransition = new FillTransition();
             fillTransition.setShape(arr[a[i]]);
             fillTransition.setToValue(color);
-            fillTransition.setDuration(Duration.millis(100));
+            fillTransition.setDuration(Duration.millis(VisualSortController.getDurationInMs()));
             pt.getChildren().add(fillTransition);
         }
         return pt;
     }
-    ParallelTransition colorRectNode(List<RectNode> list, Color color) {
-        ParallelTransition parallelTransition = new ParallelTransition();
+
+    SequentialTransition colorRectNode(List<RectNode> list, Color color) {
+        SequentialTransition sequentialTransition= new SequentialTransition();
 
         for (RectNode c : list) {
             FillTransition fillTransition = new FillTransition();
             fillTransition.setShape(c);
             fillTransition.setToValue(color);
-            fillTransition.setDuration(Duration.millis(100));
-            parallelTransition.getChildren().add(fillTransition);
+            fillTransition.setDuration(Duration.millis(VisualSortController.getDurationInMs()));
+            sequentialTransition.getChildren().add(fillTransition);
         }
 
-        return parallelTransition;
+        return sequentialTransition;
     }
+
     ParallelTransition swap(RectNode[] arr, int i, int j) {
         ParallelTransition parallelTransition = new ParallelTransition();
-        double dx = (VisualSortController.mainPaneWidth / VisualSortController.rectNodesSize);
+        double dx = (VisualSortController.getMainPaneWidth() / VisualSortController.getRectNodesSize());
         int dxFactor = j - i;
         parallelTransition.getChildren().addAll(arr[i].moveX(dx * dxFactor), arr[j].moveX(-dx * dxFactor));
 
@@ -51,5 +56,6 @@ public abstract class Sort {
 
         return parallelTransition;
     }
+
     public abstract ArrayList<Transition> startSort(RectNode[] arr);
 }
